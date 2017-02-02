121ed6706d78939db88274ad866b057f5805c972
Tommy <TommyCohn116@gmail.com>
2017-02-02 13:39:49 -0500

    Setup function.

---
+++ script.js
@@ -0,0 +1,24 @@
+//Constants
+var outerGearRadius = 225; //The radius of the outer gear.
+var innerGearRadius = 25; //The radius of the moving gear.
+
+//Global Variables
+var htmlElements = {};
+var context;
+
+function setup() {
+	htmlElements.outerTeeth = document.getElementById("outerTeeth");
+	htmlElements.innerTeeth = document.getElementById("innerTeeth");
+	htmlElements.drawButton = document.getElementById("drawButton");
+	htmlElements.canvas = document.getElementById("drawingArea");
+
+	context = htmlElements.canvas.getContext("2d");
+	resetCanvas();
+}
+function resetCanvas() {
+	context.setTransform(1, 0, 0, 1, 0, 0);
+	context.transform(1, 0, 0, 1, htmlElements.canvas.width/2, htmlElements.canvas.height/2);
+	context.transform(1, 0, 0, -1, 0, 0);
+}
+
+setup();
